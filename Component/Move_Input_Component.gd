4844ccf32d700b8fbabfc5f0af96f3ab01e568a0
Wahyuuu20 <wahyuu290400@gmail.com>
2024-08-04 04:04:31 -0700

    connect resource

--- Component/Move_Input_Component.gd
+++ Component/Move_Input_Component.gd
@@ -1,8 +1,10 @@
 class_name move_input_component
 extends Node
 
+@export var shipstats : ship_stats
 @export var movecomponent: move_component
 
 func _input(event):
 	var input_axis = Input.get_axis("left","right")
-	movecomponent.velocity = Vector2(input_axis*50, 0)
+	movecomponent.velocity = Vector2(input_axis * shipstats.speed, 0)
+	
